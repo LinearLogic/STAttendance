02fd32680a46212f1634b60e14cfc70e610d4cd0
Jared Heath <silkrawnstudios@gmail.com>
2013-05-01 13:24:53 -0400

    [0.3.2] Fill in Inbox Parser panel in the Emailer tab

--- src/com/veltro/stattendance/gui/pages/MainPage.java
+++ src/com/veltro/stattendance/gui/pages/MainPage.java
@@ -14,6 +14,7 @@ public class MainPage extends javax.swing.JPanel{
      */
     public MainPage() {
         initComponents();
+		receiveProgress.setVisible(false);
     }
 
     /**
@@ -26,6 +27,12 @@ public class MainPage extends javax.swing.JPanel{
         jPanel1 = new javax.swing.JPanel();
         sendPanel = new javax.swing.JPanel();
         receivePanel = new javax.swing.JPanel();
+        receiveButton = new javax.swing.JButton();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        receiveInfo1 = new javax.swing.JTextArea();
+        jScrollPane4 = new javax.swing.JScrollPane();
+        receiveInfo2 = new javax.swing.JTextArea();
+        receiveProgress = new javax.swing.JProgressBar();
         composePanel = new javax.swing.JPanel();
 
         sendPanel.setBorder(javax.swing.BorderFactory.createTitledBorder(javax.swing.BorderFactory.createEtchedBorder(), "Send Attendance Email", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Tahoma", 1, 11), new java.awt.Color(0, 0, 128))); // NOI18N
@@ -34,7 +41,7 @@ public class MainPage extends javax.swing.JPanel{
         sendPanel.setLayout(sendPanelLayout);
         sendPanelLayout.setHorizontalGroup(
             sendPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 250, Short.MAX_VALUE)
+            .addGap(0, 0, Short.MAX_VALUE)
         );
         sendPanelLayout.setVerticalGroup(
             sendPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -43,15 +50,79 @@ public class MainPage extends javax.swing.JPanel{
 
         receivePanel.setBorder(javax.swing.BorderFactory.createTitledBorder(javax.swing.BorderFactory.createEtchedBorder(), "Parse Incoming Email", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Tahoma", 1, 11), new java.awt.Color(0, 0, 128))); // NOI18N
 
+        receiveButton.setText("Parse Inbox");
+        receiveButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                receiveButtonActionPerformed(evt);
+            }
+        });
+
+        jScrollPane3.setBorder(null);
+
+        receiveInfo1.setEditable(false);
+        receiveInfo1.setBackground(new java.awt.Color(240, 240, 240));
+        receiveInfo1.setColumns(5);
+        receiveInfo1.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
+        receiveInfo1.setLineWrap(true);
+        receiveInfo1.setRows(3);
+        receiveInfo1.setTabSize(4);
+        receiveInfo1.setText("Click the button below to update the database\n   with attendance records contained in email\n\tmessages received from teachers.");
+        receiveInfo1.setBorder(null);
+        receiveInfo1.setRequestFocusEnabled(false);
+        receiveInfo1.setSelectedTextColor(new java.awt.Color(0, 0, 0));
+        receiveInfo1.setSelectionColor(new java.awt.Color(255, 255, 255));
+        jScrollPane3.setViewportView(receiveInfo1);
+
+        jScrollPane4.setBorder(null);
+
+        receiveInfo2.setEditable(false);
+        receiveInfo2.setBackground(new java.awt.Color(240, 240, 240));
+        receiveInfo2.setColumns(5);
+        receiveInfo2.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
+        receiveInfo2.setLineWrap(true);
+        receiveInfo2.setRows(1);
+        receiveInfo2.setTabSize(4);
+        receiveInfo2.setText("    After being parsed, messages are deleted.");
+        receiveInfo2.setBorder(null);
+        receiveInfo2.setRequestFocusEnabled(false);
+        receiveInfo2.setSelectedTextColor(new java.awt.Color(0, 0, 0));
+        receiveInfo2.setSelectionColor(new java.awt.Color(255, 255, 255));
+        jScrollPane4.setViewportView(receiveInfo2);
+
         javax.swing.GroupLayout receivePanelLayout = new javax.swing.GroupLayout(receivePanel);
         receivePanel.setLayout(receivePanelLayout);
         receivePanelLayout.setHorizontalGroup(
             receivePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 0, Short.MAX_VALUE)
+            .addGroup(receivePanelLayout.createSequentialGroup()
+                .addGroup(receivePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(receivePanelLayout.createSequentialGroup()
+                        .addGroup(receivePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(receivePanelLayout.createSequentialGroup()
+                                .addGap(94, 94, 94)
+                                .addComponent(receiveButton, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(receivePanelLayout.createSequentialGroup()
+                                .addContainerGap()
+                                .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 266, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(receivePanelLayout.createSequentialGroup()
+                                .addContainerGap()
+                                .addComponent(jScrollPane4, javax.swing.GroupLayout.PREFERRED_SIZE, 265, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addGap(0, 2, Short.MAX_VALUE))
+                    .addGroup(receivePanelLayout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(receiveProgress, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                .addContainerGap())
         );
         receivePanelLayout.setVerticalGroup(
             receivePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 0, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, receivePanelLayout.createSequentialGroup()
+                .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(receiveProgress, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(receiveButton)
+                .addContainerGap())
         );
 
         composePanel.setBorder(javax.swing.BorderFactory.createEtchedBorder());
@@ -60,7 +131,7 @@ public class MainPage extends javax.swing.JPanel{
         composePanel.setLayout(composePanelLayout);
         composePanelLayout.setHorizontalGroup(
             composePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 553, Short.MAX_VALUE)
+            .addGap(0, 454, Short.MAX_VALUE)
         );
         composePanelLayout.setVerticalGroup(
             composePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -82,16 +153,17 @@ public class MainPage extends javax.swing.JPanel{
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(sendPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(receivePanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addContainerGap())
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(18, Short.MAX_VALUE)
-                .addComponent(composePanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(12, 12, 12))
+                .addContainerGap()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(sendPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(receivePanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(0, 0, Short.MAX_VALUE)
+                        .addComponent(composePanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addGap(11, 11, 11))
         );
 
         jTabbedPane1.addTab("Emailer", jPanel1);
@@ -107,11 +179,25 @@ public class MainPage extends javax.swing.JPanel{
             .addComponent(jTabbedPane1)
         );
     }// </editor-fold>//GEN-END:initComponents
+
+    private void receiveButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_receiveButtonActionPerformed
+        receiveProgress.setVisible(true);
+		receiveButton.setText("Parsing...");
+		receiveButton.setEnabled(false);
+		// TODO: parse inbox and apply updates to the database and the progress bar
+    }//GEN-LAST:event_receiveButtonActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel composePanel;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JScrollPane jScrollPane4;
     private javax.swing.JTabbedPane jTabbedPane1;
+    private javax.swing.JButton receiveButton;
+    private javax.swing.JTextArea receiveInfo1;
+    private javax.swing.JTextArea receiveInfo2;
     private javax.swing.JPanel receivePanel;
+    private javax.swing.JProgressBar receiveProgress;
     private javax.swing.JPanel sendPanel;
     // End of variables declaration//GEN-END:variables
 }
